e300853d88f0a08fc68e014a48497010d78a5859
Eugene Fedotov <eugenefedoto@gmail.com>
2016-09-16 13:44:19 -0400

    Rewrite JS functions

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
-$(function() {
+var fullName, firstName, lastName;
+var title;
 
-    var getUser = function(id) {
+var getUser = function(id) {
+    var base = 'http://jsonplaceholder.typicode.com';
+    var route = '/users/' + id;
 
-        var base = 'http://jsonplaceholder.typicode.com';
-        var route = '/users/' + id;
+    $.ajax({
+        url: base + route,
+        method: 'GET',
+    }).done(function(user) {
+        fullName = user.name;
+        firstName = fullName.split(" ")[0];
+        lastName = fullName.split(" ")[1];
+        //console.log("NAME: " + firstName + " " + lastName);
+    });
+};
 
-        $.ajax({
-            url: base + route,
-            method: 'GET'
-        }).then(function(data) {
-            console.log(data);
-        });
-    };
+var getAlbum = function(id) {
+    var base = 'http://jsonplaceholder.typicode.com';
+    var route = '/albums?userId=' + id;
 
-    var getAlbum = function(id) {
+    $.ajax({
+        url: base + route,
+        method: 'GET',
+    }).done(function(album) {
+        title = album.title;
+    });
+};
 
-        var base = 'http://jsonplaceholder.typicode.com';
-        var route = '/albums?userId=' + id;
 
-        $.ajax({
-            url: base + route,
-            method: 'GET'
-        }).then(function(data) {
-            console.log(data);
-        });
-    };
+getUser(1);
+console.log("NAME: " + firstName + " " + lastName);
 
-});
