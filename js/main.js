e589e5e3c4d1dd17a70fbf290d1dc5ea01f1fd7c
Eugene Fedotov <eugenefedoto@gmail.com>
2016-09-16 13:44:19 -0400

    Drag and drop titles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
+function dragstart_handler(ev) {
+    //console.log("dragStart");
+    // Add the target element's id to the data transfer object
+    ev.dataTransfer.setData('text/plain', ev.target.id);
+    var data = event.dataTransfer.getData('text');
+    console.log(event.target);
+
+}
+
+function drop_handler(ev) {
+    ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    var data = ev.dataTransfer.getData("text");
+    ev.currentTarget.appendChild(document.getElementById(data));
+    console.log(ev.currentTarget);
+    //ev.target.appendChild(document.getElementById(data));
+}
+
 $(function() {
 
-    var getUser = function(id) {
+    function getUser(id) {
         var base = 'http://jsonplaceholder.typicode.com';
         var route = '/users/' + id;
 
@@ -12,7 +30,9 @@ $(function() {
         });
     }
 
-    var getAlbum = function(id) {
+
+
+    function getAlbum(id) {
         var base = 'http://jsonplaceholder.typicode.com';
         var route = '/albums?userId=' + id;
 
@@ -22,31 +42,23 @@ $(function() {
         }).done(function(data) {
             data.forEach(function(element) {
                 $('#col' + element.userId).append(
-                    '<div class="row"><span style="display: inline-block; width: 50px;">' + element.id + '</span><span>' + element.title + '</span></div>');
+                    '<div id=' + element.id + ' class="row" draggable="true" ondragstart="dragstart_handler(event)";><span style="display: inline-block; width: 50px;">' + element.id + '</span><span>' + element.title + '</span></div>');
             })
         });
     }
 
-    getUser(1).then(getUser(2)).then(getAlbum(1)).then(getAlbum(2));
 
-    var $dragging = null;
 
-    $(document.body).on("mousemove", function(e) {
-        if ($dragging) {
-        	$(e).addClass('.disable_text_highlighting');
-            $dragging.offset({
-                top: e.pageY,
-                left: e.pageX
-            });
-        }
+
+    getUser(1).then(getUser(2)).then(getAlbum(1)).then(getAlbum(2)).then(function() {
+        /* Events fired on the drop target */
+
+        // By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
+        $(document).on("dragover", function(event) {
+            event.preventDefault();
+        });
+
     });
 
 
-    $(document.body).on("mousedown", "span:last-child", function (e) {
-        $dragging = $(e.target);
-    });
-
-    $(document.body).on("mouseup", function (e) {
-        $dragging = null;
-    });
 });
